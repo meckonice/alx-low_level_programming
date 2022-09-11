63269d5f0407cc4f9490a63cc7bd9c3bb8a1a192
meckonice <meckonice93@gmail.com>
2022-09-10 19:54:18 -0700

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point: "Programming is like building a multilingual puzzle"
+ * return 0 - to exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multlingual puzzle");
+	return (0);
+}
