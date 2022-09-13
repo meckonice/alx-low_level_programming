5ad83bb5058b6e2e69e7975d5f89b43dec0fe66e
meckonice <meckonice93@gmail.com>
2022-09-13 12:12:16 -0700

    Putchar

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+	{
+		_putchar(string[n]);
+		++n
+	}
+
+	_putchar(10);
+	return (0);
+
+
+}
