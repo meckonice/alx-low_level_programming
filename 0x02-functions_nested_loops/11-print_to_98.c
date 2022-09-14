4b06a9e0930676a07a0624ef82273d88c1a5ccc1
meckonice <meckonice93@gmail.com>
2022-09-14 10:06:46 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print natural numbers to 98
+ *
+ * @n: parameter
+ *
+ * Return: natural numbers
+ *
+ */
+
+void print_to_98(int n)
+{
+	int upper = 98;
+
+	if (n > upper)
+	{
+		while (n >= upper)
+		{
+			if (n != upper)
+			{
+				printf("%d%s", n, ", ");
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= upper)
+		{
+			if (n != upper)
+			{
+				printf("%d%s", n, ", ");
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			n++;
+		}
+	}
+	putchar(10);
+}
