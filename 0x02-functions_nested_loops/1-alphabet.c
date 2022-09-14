e7265c34d489f131d41dee39f94516e849572236
meckonice <meckonice93@gmail.com>
2022-09-14 03:44:48 -0700

    Small alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: print small alphabetical letters
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
