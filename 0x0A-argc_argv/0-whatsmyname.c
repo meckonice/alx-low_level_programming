115f62786531281726540ccb7efb8c7d4260d18a
meckonice <meckonice93@gmail.com>
2022-09-30 11:40:23 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 /**
- * main - prints the name of the program
- * @argc: the number of arguments to the program
- * @argv: the pointers to the arguments
- * Return: 0
+ * main - Entry point with arguments
+ *
+ * @argc: counter of arguments passed
+ * @argv: vector of strings with arguments passed
+ *
+ * Return: Always 0 (Success)
+ *
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
