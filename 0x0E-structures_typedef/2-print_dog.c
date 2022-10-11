591184101ea71aa96cc14db5d09c59b6b864cc80
meckonice <meckonice93@gmail.com>
2022-10-10 22:46:28 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,42 +1,31 @@
-#include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-/**
- * print_dog - prints the contents of the struct dog
- * @d: a pointer to a dog struct
- * Return: void
- */
-void print_dog(struct dog *d)
-{
-	char *name;
-	char *owner;
 
-	if (d == NULL)
-		printf(" ");
-	else
-	{
-		name = malloc(sizeof(d->name));
-		if (name == NULL)
-			printf("Name: (nil)\n");
-		else
-		{
-			strcpy(name, d->name);
-			printf("Name: %s\n", name);
-		}
-		if (d->age != 0)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age :(nil)\n");
-		owner = malloc(sizeof(d->owner));
-		if (owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-		{
-			strcpy(owner, d->owner);
-			printf("Owner: %s\n", owner);
-		}
-		free(name);
-		free(owner);
-	}
+#include "dog.h"
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+	struct dog my_dog;
+
+
+
+	my_dog.name = "Poppy";
+
+	my_dog.age = 3.5;
+
+	my_dog.owner = "Bob";
+
+	print_dog(&my_dog);
+
+	return (0);
+
 }
