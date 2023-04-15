214d59f7ab88f501ab2dc7066abddc3996a4392a
meckonice <meckonice93@gmail.com>
2023-04-14 18:07:59 -0700

    search_algos.h

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
  * linear_search - function that searches for a value in an array
  * of integers using the Linear search algorithm
  *
- * @array: is a pointer to the first element of the array to search in 
+ * @array: is a pointer to the first element of the array to search in
  * @size: is the number of element in array
  * @value: is the value to search for
  *
